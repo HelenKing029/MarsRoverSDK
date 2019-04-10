5db7c0f3c9bbddf862d8a9d841f56bfb284493b6
Helen K <helenking029@gmail.com>
2019-04-10 08:04:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-api_key = "ENTER-API-KEY"
-
 import requests
 import webbrowser
 
-def myrequest(request):
-    url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol={}&camera={}&api_key={}"
-    sol = 1000
-    camera = fhaz
-    response = requests.get(url.format(sol, camera, api_key)).json()
-    photos = response["photos"]
-    return webbrowser.open_new_tab(roverImage)
+api_key = "wTdsNpFtovzuN4I2Vo4xGU3SQLf8e5robl4oj9c6"
 
-#Get request to retreive data and convert to json
-myData = requests.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=ENTER_YOUR_KEY")
-json_dict = myData.json()
-#print(json_dict)
+def myrequest():
+    url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key={api_key}"
+    myData = requests.get(url)
+    print(myData)
+    json_dict = myData.json()
+    print(json_dict)
+    roverImage = json_dict["photos"][0]["img_src"]
+    webbrowser.open_new_tab(roverImage)
 
-#get the image from first place holder
-roverImage = json_dict["photos"][0]["img_src"]
-print(roverImage)
-
-#open image in web browser
-webbrowser.open_new_tab(roverImage)
-
-
-
-
-"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=ENTER_YOUR_KEY"
+myrequest()
+#"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=ENTER_YOUR_KEY"

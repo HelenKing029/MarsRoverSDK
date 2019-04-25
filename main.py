22b97d60b5c9553902137067b0f82dda57289b21
Helen <helenking029@gmail.com>
2019-04-25 08:32:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
-api_key = "ENTER-API-KEY"
-
 import requests
 import webbrowser
 
-def myrequest(request):
-    url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol={}&camera={}&api_key={}"
-    sol = 1000
-    camera = fhaz
-    response = requests.get(url.format(sol, camera, api_key)).json()
-    photos = response["photos"]
+api_key = "ENTER-KEY"
+
+
+def mostRecentSolDate():
+    url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=max_sol&camera=fhaz&api_key={}".format(api_key)
+    myData = requests.get(url)
+    print(myData)
+    json_dict = myData.json()
+    print(json_dict)
+    roverImage = json_dict["photos"][0]["img_src"]
     return webbrowser.open_new_tab(roverImage)
 
-#Get request to retreive data and convert to json
-myData = requests.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=ENTER_YOUR_KEY")
-json_dict = myData.json()
-#print(json_dict)
+def roverMissionStatus():
+    url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?status&api_key={}".format(api_key)
+    myData = requests.get(url)
+    roverStatus = myData.json()
+    print(roverStatus)
 
-#get the image from first place holder
-roverImage = json_dict["photos"][0]["img_src"]
-print(roverImage)
+def missionManifest(rover_name):
+    url = "https://api.nasa.gov/mars-photos/api/v1/manifests/{}?api_key={}".format(rover_name, api_key)
+    myData = requests.get(url).json()
+    manifest = myData['photo_manifest'] ## need to confirm if this is correct?
+    print(manifest)
 
-#open image in web browser
-webbrowser.open_new_tab(roverImage)
-
-
-
-
-"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=ENTER_YOUR_KEY"
+missionManifest(curiosity)
+myrequest()
+#"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=ENTER_YOUR_KEY"
